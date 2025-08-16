086975e00d28d208eac06b7a9d3318d4b1da013c
Adriankf59 <96868524+Adriankf59@users.noreply.github.com>
2025-08-16 07:52:44 +0700

    Delete .history directory

--- .history/components/MapComponent_20250509032321.js
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
-import Head from 'next/head';
-
-// Create a placeholder while the map is loading
-const MapLoadi
--- .history/next-sitemap.config_20250624072733.js
+++
@@ -1,7 +0,0 @@
-// next-sitemap.config.js
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://arsipastacala.web.id',
-  generateRobotsTxt: true, // Opsional: ini juga akan membuatkan robots.txt untuk Anda
-  // ...opsi lainnya
-}
